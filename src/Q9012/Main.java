65bf1209026ecf7b53b7b76c8ecaf438a20ebd7e
Choi Seong ho <dev-csh@naver.com>
2021-05-11 23:34:41 +0900

    add

--- src/Q9012/Main.java
+++ src/Q9012/Main.java
@@ -10,35 +10,36 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int test = Integer.parseInt(br.readLine());
-		
 		ArrayList<String> arr = new ArrayList<String>();
-		
-		
-		for(int i = 0; i < test; i++) {
-			String temp = br.readLine();
-			arr.add(temp);
-		}
-		
-		for(String str : arr) {
-			char[] chs = str.toCharArray();
 
+		for (int i = 0; i < test; i++) {
+			arr.add(br.readLine());
+		}
+
+		for (String str : arr) {
+			boolean vps = true;
 			int cnt1 = 0;
 			int cnt2 = 0;
-			for(int i = 0; i<chs.length; i++) {
-				
-				if(chs[i] == '(') cnt1 += 1;
-				
-				if(chs[i] == ')') cnt2 += 1;
-				
+
+			for (int i = 0; i < str.length(); i++) {
+
+				if (str.charAt(i) == '(') {
+					cnt1++;
+				} else if (str.charAt(i) == ')') {
+					cnt2++;
+
+					if (cnt2 > cnt1) {
+						vps = false;
+						break;
+					}
+				}
 			}
 
-			if(cnt1 == cnt2) {
-				
-				
+			if (vps && cnt1 == cnt2)
 				System.out.println("YES");
-			}
-			else System.out.println("NO");
-			
+			else
+				System.out.println("NO");
+
 		}
 	}
 }
