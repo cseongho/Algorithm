cb3f7d7d074286a7dbb578918bbd025869da449e
cseongho <dev-csh@naver.com>
2021-06-09 13:46:08 +0900

    Add test package

---
+++ src/test/test/Main.java
@@ -0,0 +1,38 @@
+package test.test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		//입출력 및 StringTokenizer
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        int N= Integer.parseInt(st.nextToken());
+        int M= Integer.parseInt(st.nextToken());
+        bw.write(N + " " + M);
+		
+		//문자열 뒤집기
+		String str = "Reverse";
+		str = new StringBuilder(str).reverse().toString();
+		bw.write(str);
+
+		//문자열 변경
+		String name = "starfucks";
+		String rename = name.substring(0,4)+'b' + name.substring(5);
+		bw.write(rename);
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
