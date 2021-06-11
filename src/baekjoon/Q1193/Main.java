48e54a9a9e46261f1816b8134b6d4e9f0cd29ea6
cseongho <dev-csh@naver.com>
2021-06-11 11:56:55 +0900

    sovle & add algorithm

---
+++ src/baekjoon/Q1193/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.Q1193;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int input = Integer.parseInt(br.readLine());
+	}
+}
---
+++ src/study/algorithm/Permutation.java
@@ -0,0 +1,22 @@
+package study.algorithm;
+
+public class Permutation {
+	static int N = 4;
+	static int[] Nums = {1,2,3,4};
+	
+	static int solve(int pos, int cnt, int val) {
+		if (cnt == 2) return val;
+		
+		int ret = 0;
+		ret = Math.max(ret, solve(pos + 1, cnt + 1, val + Nums[pos]));
+		ret = Math.max(ret, solve(pos + 1, cnt, val));
+		
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solve(0, 0, 0));
+	}
+	
+	//Combining the two largest numbers.
+}
