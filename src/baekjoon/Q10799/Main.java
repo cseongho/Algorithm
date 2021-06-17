61a64b3dcc18b0d64aa8471859acf238ae7a29c9
cseongho <dev-csh@naver.com>
2021-06-17 15:20:46 +0900

    sovle

--- src/baekjoon/Q10799/Main.java
+++ src/baekjoon/Q10799/Main.java
@@ -3,22 +3,39 @@ package baekjoon.Q10799;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+
+import javax.security.sasl.SaslException;
 
 public class Main {
 	static String input;
-	static int cnt = 0;
+	static int result = 0;
+	static Stack<Character> stack = new Stack<>();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		input = br.readLine();
+		int length = input.length();
 		
-		for(int i = 0; i < input.length(); i++) {
-			if(i+1 == input.length()) break;
-			if(input.charAt(i) == '(' && input.charAt(i+1) == ')' ) {
-				cnt++;
+		for(int i = 0; i < length; i++) {
+			if(input.charAt(i) == '(') {
+				stack.push('(');
+				continue;
+			}
+			
+			if(input.charAt(i) == ')' ) {
+				stack.pop();
+				
+				if(input.charAt(i - 1) == '(') {
+					result += stack.size();
+				} else {
+					result++;
+				}
+				
 			}
 		}
+		System.out.println(result);
 		
 	}
 }
