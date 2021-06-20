d16e316490ebddd3f3a04bd43adbc686bff2a53f
cseongho <dev-csh@naver.com>
2021-06-20 14:20:03 +0900

    solve

---
+++ src/baekjoon/Q11650/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.Q11650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[][] arr;	
+	static ArrayList<int [][]> arry = new ArrayList<int [][]>();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (e1,e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i][0] + " ").append(arr[i][1]).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
