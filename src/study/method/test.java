1ab5039b27467a40e21cdb93c3cfdb14261b67a9
cseongho <dev-csh@naver.com>
2021-06-22 16:44:02 +0900

    solve

--- src/study/method/test.java
+++ src/study/method/test.java
@@ -1,17 +1,30 @@
 package study.method;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class test {
 
-	public static void main(String[] args) {
-		PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-		pq.add(2);pq.add(12);pq.add(13);
-
-		System.out.println(pq);
-		pq.remove();
-		System.out.println(pq);
+	public static void main(String[] args) {		
+		int[] numbers = {6,10,2};
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
+		for(int i = 0; i < numbers.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			String prefix = Integer.toString(numbers[i]);
+			sb.append(prefix);
+			
+			for(int j = numbers.length - 1; j >= 0; j--) {
+				if(i == j) continue;
+				sb.append(Integer.toString(numbers[j]));
+			}
+			arr.add(Integer.parseInt(sb.toString()));
+		}
+		
+		for(int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}
 
 	}
 
