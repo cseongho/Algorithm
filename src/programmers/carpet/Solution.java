873faa666c04edc5975fc7c2bfaaf8a4352a169f
cseongho <dev-csh@naver.com>
2021-06-23 02:11:55 +0900

    solve

---
+++ src/programmers/carpet/Solution.java
@@ -0,0 +1,31 @@
+package programmers.carpet;
+
+class Solution {
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int area = brown + yellow;
+        
+        for(int i = 1; i <= area; i++) {
+            int row = i;
+            int col = area / row;
+            
+            if(row > col)
+                continue;
+            if((row - 2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            } 
+        }
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		int[] answer = solution(10, 2);
+		
+		for(int i = 0; i < answer.length; i++) {
+			System.out.print(answer[i]);
+		}
+	}
+}
