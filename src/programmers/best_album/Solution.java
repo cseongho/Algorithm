47ba7e059fc6118b3f14348c3de51f67d6da8113
cseongho <dev-csh@naver.com>
2021-06-23 15:27:07 +0900

    solve

--- src/programmers/best_album/Solution.java
+++ src/programmers/best_album/Solution.java
@@ -1,36 +1,63 @@
 package programmers.best_album;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+ 
+/* https://tosuccess.tistory.com/40 참고 */
 
 public class Solution {
-	public static int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+    public static int[] solution(String[] genres, int[] plays) {
         
-//        Arrays.sort(genres);
-//        Arrays.sort(plays);
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();        //장르별 재생횟수를 구하기 위해 hashmap사용
+        for(int i = 0; i < genres.length; i++) {                             //hashmap에 장르별 플레이 횟수 저장
+            hm.put(genres[i], hm.getOrDefault(genres[i], 0) + plays[i]);
+        }
+        ArrayList<Integer> answers = new ArrayList<Integer>();                //나중에 int[]형으로 바꿔서 리턴할 arrayList
+        ArrayList<String> al = new ArrayList<String>(hm.keySet());            //Value를 내림차순으로 정리하기 위해 정의
+        Collections.sort(al, (o1,o2)->(hm.get(o2).compareTo(hm.get(o1))));    //ArrayList를 hm Value 내림차순 순으로 정렬
         
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        
-        for(int i = 0; i < genres.length; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+        for(int i = 0; i < al.size(); i++) {                                //비교, 정답 추출 실행문
+            String TempGenres = al.get(i);
+            int firstIdx = 0;
+            int secondIdx = 0;
+            int max = 0;
+            for(int j = 0; j < genres.length; j++) {    //첫번째 값 구하기
+                if(TempGenres.equals(genres[j])) {        //높은 순으로 가져온 장르와 genres배열의 값이 같으면
+                    if(plays[j]> max) {                    //max와 비교해서 최댓값 갱신
+                        max = plays[j];
+                        firstIdx = j;
+                    }
+                }
+            }
+            
+            max = -1;                                     //장르가 하나일 수 있으므로 -1
+            for(int j = 0; j < genres.length; j++) {    //두번째 값 구하기
+                if(TempGenres.equals(genres[j])) {
+                    if(j != firstIdx && plays[j] > max) {
+                        max = plays[j];
+                        secondIdx = j;
+                    }
+                }
+            }
+            answers.add(firstIdx);            //제일 큰 값 추가
+            if(max != -1) {
+            answers.add(secondIdx);            //다음 큰 값 추가
+            }
         }
         
-        String[] temp = new String[map.size()];
-        for(String key : map.keySet()) {
-        	
-        }
+        return answers.stream().mapToInt(Integer::intValue).toArray();
         
-        
-        
-        System.out.println(map.toString());
-        
-        return answer;
     }
-	
-	public static void main(String[] args) {
-		String[] genres = { "classic", "pop", "classic", "classic", "pop"};
-		int[] plays = { 500, 600, 150, 800, 2500};
-		
-		solution(genres, plays);
+    
+    public static void main(String[] args) {
+    	String[] genres = { "classic", "pop", "classic", "classic", "pop"};
+    	int[] plays = {500, 600, 150, 800, 2500};
+    	int[] answers = solution(genres, plays);
+    	
+    	for(int i = 0; i < answers.length; i++) {
+    		System.out.print(answers[i] + " ");
+    	}
+
 	}
 }
